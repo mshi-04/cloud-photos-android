95ece2694bddde6dbc7dff4169505a29b0f073ce
masato <ma.whwell4@gmail.com>
2026-02-28 20:10:02 +0900

    AWSのCognito設定を追加

--- app/src/main/kotlin/com/appvoyager/cloudphotos/CloudPhotosApp.kt
+++ app/src/main/kotlin/com/appvoyager/cloudphotos/CloudPhotosApp.kt
@@ -1,7 +1,23 @@
 package com.appvoyager.cloudphotos
 
 import android.app.Application
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
+import com.amplifyframework.core.Amplify
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CloudPhotosApp : Application()
+class CloudPhotosApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        try {
+            Amplify.addPlugin(AWSCognitoAuthPlugin())
+            Amplify.configure(applicationContext)
+        } catch (error: AmplifyException) {
+            throw RuntimeException("Amplify initialization failed. Check amplifyconfiguration.json.", error)
+        }
+    }
+
+}
