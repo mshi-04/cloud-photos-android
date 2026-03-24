d350a8c710698d0d717b91c947b381a38f2cf7aa
masato <ma.whwell4@gmail.com>
2026-03-24 18:16:19 +0900

    通知タップ時の画面遷移の追加と不要なTAGの削除

--- app/src/main/kotlin/com/appvoyager/cloudphotos/fcm/CloudPhotosFirebaseMessagingService.kt
+++ app/src/main/kotlin/com/appvoyager/cloudphotos/fcm/CloudPhotosFirebaseMessagingService.kt
@@ -2,12 +2,15 @@ package com.appvoyager.cloudphotos.fcm
 
 import android.Manifest
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.appvoyager.cloudphotos.data.fcm.DeviceToken
+import com.appvoyager.cloudphotos.ui.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import dagger.hilt.android.AndroidEntryPoint
@@ -36,11 +39,21 @@ class CloudPhotosFirebaseMessagingService : FirebaseMessagingService() {
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            NOTIFICATION_ID,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(android.R.drawable.ic_dialog_info)
             .setContentTitle(title)
             .setContentText(body)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
             .setAutoCancel(true)
             .build()
 
@@ -58,7 +71,6 @@ class CloudPhotosFirebaseMessagingService : FirebaseMessagingService() {
     companion object {
         const val CHANNEL_ID = "upload_complete"
         private const val NOTIFICATION_ID = 2001
-        private const val TAG = "CloudPhotosFirebaseMessagingService"
     }
 
 }
