237e1a2af34ec307b830883347edf0476ac39946
masato <ma.whwell4@gmail.com>
2026-03-25 14:35:13 +0900

    Amplify REST API呼び出しの共通化とサインアウトのユニットテスト追加

---
+++ core/data/src/main/kotlin/com/appvoyager/cloudphotos/data/common/AmplifyRestClient.kt
@@ -0,0 +1,30 @@
+package com.appvoyager.cloudphotos.data.common
+
+import com.amplifyframework.api.ApiException
+import com.amplifyframework.api.rest.RestOptions
+import com.amplifyframework.api.rest.RestResponse
+import com.amplifyframework.core.Consumer
+import com.amplifyframework.core.async.Cancelable
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resumeWithException
+
+suspend fun awaitAmplifyRestCall(
+    options: RestOptions,
+    apiCall: (String, RestOptions, Consumer<RestResponse>, Consumer<ApiException>) -> Cancelable?
+): RestResponse = suspendCancellableCoroutine { coroutine ->
+    val operation = apiCall(
+        AMPLIFY_API_NAME,
+        options,
+        Consumer { apiResponse ->
+            if (apiResponse.code.isSuccessful) {
+                coroutine.resume(apiResponse) { _, _, _ -> }
+            } else {
+                coroutine.resumeWithException(
+                    Exception("Unexpected response code ${apiResponse.code}: ${apiResponse.data.asString()}")
+                )
+            }
+        },
+        Consumer { coroutine.resumeWithException(it) }
+    )
+    coroutine.invokeOnCancellation { operation?.cancel() }
+}
--- core/data/src/main/kotlin/com/appvoyager/cloudphotos/data/fcm/DeviceTokenDataSource.kt
+++ core/data/src/main/kotlin/com/appvoyager/cloudphotos/data/fcm/DeviceTokenDataSource.kt
@@ -6,11 +6,9 @@ import com.amplifyframework.api.rest.RestResponse
 import com.amplifyframework.core.Amplify
 import com.amplifyframework.core.Consumer
 import com.amplifyframework.core.async.Cancelable
-import com.appvoyager.cloudphotos.data.common.AMPLIFY_API_NAME
-import kotlinx.coroutines.suspendCancellableCoroutine
+import com.appvoyager.cloudphotos.data.common.awaitAmplifyRestCall
 import org.json.JSONObject
 import javax.inject.Inject
-import kotlin.coroutines.resumeWithException
 
 class DeviceTokenDataSource @Inject constructor() {
 
@@ -20,12 +18,7 @@ class DeviceTokenDataSource @Inject constructor() {
             put("platform", "android")
         }.toString()
         callDeviceApi(body) { name, opts, onResp, onErr ->
-            Amplify.API.put(
-                name,
-                opts,
-                onResp,
-                onErr
-            )
+            Amplify.API.put(name, opts, onResp, onErr)
         }
     }
 
@@ -34,41 +27,20 @@ class DeviceTokenDataSource @Inject constructor() {
             put("deviceToken", token.value)
         }.toString()
         callDeviceApi(body) { name, opts, onResp, onErr ->
-            Amplify.API.delete(
-                name,
-                opts,
-                onResp,
-                onErr
-            )
+            Amplify.API.delete(name, opts, onResp, onErr)
         }
     }
 
     private suspend fun callDeviceApi(
         body: String,
-        performCall: (String, RestOptions, Consumer<RestResponse>, Consumer<ApiException>) -> Cancelable?
+        apiCall: (String, RestOptions, Consumer<RestResponse>, Consumer<ApiException>) -> Cancelable?
     ) {
-        val restOptions = RestOptions.builder()
+        val options = RestOptions.builder()
             .addPath("/devices/token")
             .addHeaders(mapOf("Content-Type" to "application/json"))
             .addBody(body.toByteArray())
             .build()
-        suspendCancellableCoroutine { coroutine ->
-            val operation = performCall(
-                AMPLIFY_API_NAME,
-                restOptions,
-                { apiResponse ->
-                    if (apiResponse.code.isSuccessful) {
-                        coroutine.resume(Unit) { _, _, _ -> }
-                    } else {
-                        coroutine.resumeWithException(
-                            Exception("Unexpected response code ${apiResponse.code}: ${apiResponse.data.asString()}")
-                        )
-                    }
-                },
-                { coroutine.resumeWithException(it) }
-            )
-            coroutine.invokeOnCancellation { operation?.cancel() }
-        }
+        awaitAmplifyRestCall(options, apiCall)
     }
 
 }
