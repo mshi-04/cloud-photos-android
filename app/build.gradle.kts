81e1a118f3bfcf39c15140b4808666a4a983b8c7
masato <ma.whwell4@gmail.com>
2026-03-26 18:52:27 +0900

    ui.tooling.previewの依存関係を追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,6 +97,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.ui.tooling.preview)
     debugImplementation(libs.androidx.ui.tooling)
 
     // DI
